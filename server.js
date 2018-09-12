61eb0fd45dcb1f3703e59d673b43b70609f6823f
dima Tikhonov <dima1mim@gmail.com>
2018-09-12 17:50:40 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + './herelo/dist/'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'./herelo/dist//index.html'));
+app.get('/*all', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
