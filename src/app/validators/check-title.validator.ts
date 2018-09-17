8327d6a52391ec292601f54c83955b3ad8afee48
Dmitri Tikhonov <dima1mim@gmail.com>
2018-09-17 16:10:00 +0300

    Update check-title.validator.ts

--- src/app/validators/check-title.validator.ts
+++ src/app/validators/check-title.validator.ts
@@ -1,9 +1,11 @@
 import { FormControl } from '@angular/forms';
 
-export const validNameTitle = (books:any) => {
+export const validNameTitle = (books:any,titleCurrent:string) => {
   return (control: FormControl) => {
     if( books.some(x => x.title === control.value)){
+      if(titleCurrent == '' || titleCurrent !== control.value){
         return ({validTitle: true});
+        }
       }else{
         return null;
       }
